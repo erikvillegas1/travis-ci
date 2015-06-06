6826904b876e93bc677f1532d664cf3204694aec
Erik Villegas <erikvillegas@eat24.com>
2015-06-05 20:06:41 -0700

    Initial Commit

---
+++ TravisTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TravisTest
+//
+//  Created by Erik Villegas on 6/5/15.
+//  Copyright (c) 2015 Erik Villegas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TravisTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TravisTest
+//
+//  Created by Erik Villegas on 6/5/15.
+//  Copyright (c) 2015 Erik Villegas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
